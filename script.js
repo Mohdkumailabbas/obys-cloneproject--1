8dd01d4293fb5b620319e4503c17811402ce5d19
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2024-05-10 13:43:29 +0530

    locomotive applied

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+function locomotiveanime(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+} 
 gsap.from(".line h1",{
     y:150,
     stagger:0.2,
@@ -46,6 +77,10 @@ tl.from("#middle1 h1, #middle2 h1, #middle3 h2 ,  #middle4 h1 ",{//makin appear 
     y:140,
     stagger:0.2
 })
+tl.from("#middle1,#page2",{
+  opacity:0,
+},"-=1.2");/*page 1 ke content ka anime fst kr diya*/
+
 function cursoranime(){
     //code for cursor
 document.addEventListener("mousemove",function(dets){
@@ -60,3 +95,4 @@ document.addEventListener("mousemove",function(dets){
     });
 }
 cursoranime()
+locomotiveanime()
